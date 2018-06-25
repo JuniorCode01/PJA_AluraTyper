ae72b28fa3e7b85ec3bb62398d6970049f213699
Junior Araújo <jrbyte01@gmail.com>
2018-06-25 11:18:31 -0300

    Mudando o nome de algumas variáveis de acordo com as aulas correspondentes

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,13 @@
 var frase = $(".frase").text(); //obtem do DOM um objeto com a classe frase e obtendo o valor de texto atribuindo para uma variável
-var contadorPalavras = frase.split(" ").length; //uma variável recebe o resultado que pega cada palavra e separa pelos espaços, joga em um array e obtem o seu tamanho.
+var numPalavras = frase.split(" ").length; //uma variável recebe o resultado que pega cada palavra e separa pelos espaços, joga em um array e obtem o seu tamanho.
 
-var contadorAtual = $("#tamanho-frase"); //assim que é feito a contagem, obtem um objeto do DOM com o id cont,
-contadorAtual.text(contadorPalavras); //substituindo seu valor de texto para o número de palavras contadas.
+var tamanhoFrase = $("#tamanho-frase"); //assim que é feito a contagem, obtem um objeto do DOM com o id cont,
+contadorAtual.text(numPalavras); //substituindo seu valor de texto para o número de palavras contadas.
 
 
 var campo = $(".campo-digitacao"); //obtendo o conteúdo de dento do text area através do evento de input
 campo.on("input", function() {
-    conteudo = campo.val();
-    console.log(conteudo);
+    var conteudo = campo.val();
     
     //Depois que o browser reconhece o evento de input, obtemos o valor do seu conteúdo,
     //assim com a função split podemos usar expressão regular que remove os espaços.
